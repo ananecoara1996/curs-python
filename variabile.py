65d7c90c008d45ba3d1eee18a470917cf4f01150
Ana Necoara <ana.necoara1996@gmail.com>
2025-12-15 15:09:57 +0200

    Create variabile.py

---
+++ variabile.py
@@ -0,0 +1,81 @@
+from re import U
+
+
+varsta = "Treizeci de ani " 
+print (varsta)
+
+variabila_a = 10 
+variabila_b = 20
+
+print (variabila_a+variabila_b)
+
+nume_familie = 'Necoara '
+prenume_cursant = 'Ana-Maria'
+
+print (nume_familie + prenume_cursant)
+
+var1 = 10
+var2 = "test"
+var3 = 7.3
+var4 = True
+
+var_type = type(var1)
+print(var_type)
+var_type = type(var2)
+print(var_type)
+var_type = type(var3)
+print(var_type)
+var_type = type(var4)
+print(var_type)
+
+
+var = "10"
+print(var)
+print (type(var))
+
+var = int(var)
+print(var)
+print (type(var))
+
+var = float(var)
+print(var)
+print (type(var))
+
+var1 = "10"
+var2 = "30"
+
+rezultat = var1 +var2
+print (rezultat)
+
+var7 = 10
+var8 = 10.5 
+
+print (var7+int(var8))
+print (type(var7+int(var8)))
+
+vari1 = ''  #string gol -> duce la False 
+vari2 = 'ce frumos'
+
+vari1 = bool(vari1)
+vari2 = bool(vari2)
+
+print (vari1)
+print (vari2)
+
+print ('Hello world! Python is cool!')
+
+print (92%3)
+print (9**6)
+
+print (99/7)
+print (99//7)
+
+print (7 * (12-6) - 2**3)
+print (42-8)
+
+#name = input ("Care este numele tau? ")
+#print('Salut, ' + name)
+
+print (2**4)
+grecesc = "αβγδεζηθικλμνξοπρςστυφχψ"
+print(grecesc)
